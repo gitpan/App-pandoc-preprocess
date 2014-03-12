e6990d168034f0a2e761db1f3915097f810b3d79
Daniel <dbr@cpan.org>
2014-03-12 22:57:17 +0000

    Import of DBR/App-pandoc-preprocess-0.7.1 from CPAN.
    gitpan-cpan-distribution: App-pandoc-preprocess
    gitpan-cpan-version:      0.7.1
    gitpan-cpan-path:         DBR/App-pandoc-preprocess-0.7.1.tar.gz
    gitpan-cpan-author:       DBR
    gitpan-cpan-maturity:     released

--- lib/App/pandoc/preprocess.pm
+++ lib/App/pandoc/preprocess.pm
@@ -3,7 +3,7 @@ BEGIN {
   $App::pandoc::preprocess::AUTHORITY = 'cpan:DBR';
 }
 {
-  $App::pandoc::preprocess::VERSION = '0.7.0';
+  $App::pandoc::preprocess::VERSION = '0.7.1';
 }
 
 #  PODNAME: App::pandoc::preprocess
@@ -19,7 +19,7 @@ App::pandoc::preprocess - Preprocess Pandoc before Processing Pandoc
 
 =head1 VERSION
 
-version 0.7.0
+version 0.7.1
 
 =head1 ppp - pandoc pre-process
 
